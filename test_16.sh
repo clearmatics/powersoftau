1549c64afb054067aa82bb3689e3ff4420aab7fc
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-08-06 12:45:44 +0100

    small test script

---
+++ test_16.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -e
+
+FLAGS="--release -- -n 16"
+
+# Run 3 rounds and the beacon
+rm -f challenge response
+
+cargo run --bin new ${FLAGS}
+# Wrote a fresh accumulator to `./challenge`
+
+# Round 1
+
+echo random1 | cargo run --bin compute ${FLAGS} --digest response.1.digest
+# Your contribution has been written to `./response`
+
+cargo run --bin verify_transform ${FLAGS}
+# Verification succeeded! Writing to `./new_challenge`
+
+mv challenge challenge.0
+mv response response.1
+mv new_challenge challenge
+
+# Round 2
+
+echo random2 | cargo run --bin compute ${FLAGS}
+# Your contribution has been written to `./response`
+
+cargo run --bin verify_transform ${FLAGS}
+# Verification succeeded! Writing to `./new_challenge`
+
+mv challenge challenge.1
+mv response response.2
+mv new_challenge challenge
+
+# Round 3
+
+echo random3 | cargo run --bin compute ${FLAGS}
+# Your contribution has been written to `./response`
+
+cargo run --bin verify_transform ${FLAGS}
+# Verification succeeded! Writing to `./new_challenge`
+
+mv challenge challenge.2
+mv response response.3
+mv new_challenge challenge
+
+# Round 4 (Beacon)
+
+echo random4 | cargo run --bin compute ${FLAGS}
+# Your contribution has been written to `./response`
+
+cargo run --bin verify_transform ${FLAGS}
+# Verification succeeded! Writing to `./new_challenge`
+
+mv challenge challenge.3
+mv response response.4.beacon
+mv new_challenge challenge
+
+echo Creating transcript ...
+rm -f transcript
+for f in response.1 response.2 response.3 response.4.beacon ; do
+    dd if=$f bs=64 skip=1 >> transcript
+done
+cargo run --bin verify ${FLAGS} -r 4
